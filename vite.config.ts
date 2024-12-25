d89f2ebb88621061a3e34a8f742374a10087023d
Marcel Armada-Castellon <marcel.armada@armadacore.com>
2024-12-25 22:00:32 +0100

    refactor: Update library entry point from main.ts to index.ts
    Refactored the library entry file for consistency, renaming `main.ts` to `index.ts` and updating references in `vite.config.ts` and `package.json`. Adjusted output file naming to align with the new entry point.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import {defineConfig} from "vite";
-import {resolve} from 'path';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-    plugins: [dts({include: ['src']})],
-    build: {
-        copyPublicDir: false,
-        lib: {
-            entry: resolve(__dirname, 'src/main.ts'),
-            fileName: 'main',
-            formats: ['es'],
-        }
-    }
-})
+	plugins: [dts({ include: ['src'] })],
+	build: {
+		copyPublicDir: false,
+		lib: {
+			entry: resolve(__dirname, 'src/index.ts'),
+			fileName: 'index',
+			formats: ['es'],
+		},
+	},
+});
